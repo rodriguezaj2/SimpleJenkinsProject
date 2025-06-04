91d9aa8d49fda89b13e91eb7a68ae888ebbd4fd4
rodriguezaj2 <rodriguezaj2020@gmail.com>
2025-06-04 17:20:15 -0400

    Runs 15 times now

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println("Hello world!");
             Thread.sleep(2000);
         }
